53cda414afcf362a37c5a7547ca78782160c6d52
Takehiro Adachi <takehiro0740@gmail.com>
2022-07-09 15:45:05 +0900

    Move input and output files under example directory

--- grouping.rb
+++ grouping.rb
@@ -1,10 +1,10 @@
 require 'csv'
 require './lib/people_csv.rb'
 
-input_filenames = [
-  'input1.csv',
-  'input2.csv',
-  'input3.csv',
+input_files = [
+  'examples/inputs/input1.csv',
+  'examples/inputs/input2.csv',
+  'examples/inputs/input3.csv',
 ]
 
 # csv           - value of CSV.read()
@@ -13,15 +13,15 @@ def group(csv, matching_type)
   PeopleCSV.new(csv).group_by_matching_type(matching_type)
 end
 
-input_filenames.each do |input_filename|
-  csv = CSV.read(input_filename)
-  filename_wo_extension = input_filename.split('.').first
+input_files.each do |input_file|
+  csv = CSV.read(input_file)
+  filename_wo_extension = input_file.split('/').last.split('.').first
 
   PeopleCSV::MATCHING_TYPES.each do |matching_type|
     grouped_csv = group(csv, matching_type)
 
     CSV.open(
-      "outputs/#{filename_wo_extension}_grouped_by_#{matching_type.to_s}.csv",
+      "examples/outputs/#{filename_wo_extension}_grouped_by_#{matching_type.to_s}.csv",
       "wb"
     ) do |grouped_csv_file|
       grouped_csv.each do |row|
